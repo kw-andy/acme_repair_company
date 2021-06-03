a50ecda70ecdc186635440d951b2a11f488d01ea
Andy Kwok <akwok@anybox.fr>
2021-06-03 16:00:26 +0200

    code nettoyé

--- repairs_parts.py
+++ repairs_parts.py
@@ -2,115 +2,210 @@ import requests
 import json
 import os
 
+"""[How to use the script]
+
+the URL variable must be set in your bash prompt (see next line)
+make sure that variable URL does not end with /
+(YES: vroomly.com | NO : vroomly.com/)
+export URL='https://www.vroomly.com/backend_challenge/labour_times'
+export FILENAME='data.json' #for the file input
+export FILENAME_OUTPUT='quotations.json' #for the file output
+
+"""
 
 
-#the URL variable must be set in your bash prompt (see next line)
-#export URL='https://www.vroomly.com/backend_challenge/labour_times'
-#make sure that variable URL does not end with / (YES: vroomly.com | NO : vroomly.com/)
-#export FILENAME='data.json' #for the file input
-#export FILENAME_OUTPUT='quotations.json' #for the file output
+url = os.getenv("URL")
+filename = os.getenv("FILENAME")
+filename_output = os.getenv("FILENAME_OUTPUT")
 
-URL = os.getenv('URL')
-FILENAME = os.getenv('FILENAME')
-FILENAME_OUTPUT = os.getenv('FILENAME_OUTPUT')
 
-def retrieve_hourly_rate(URL,FILENAME):
-    with open(FILENAME,'r') as jsonfile:
+def retrieve_hourly_rate(url, filename):
+    """retrieve_hourly_rate
+    allows to retrieve the hourly rate from the API
+    input: url of the API, the json file given.
+    output: dictionnary -> car_id,intervention_id,time spent
+    """
+    with open(filename, "r") as jsonfile:
         json_data = json.load(jsonfile)
-    cars = [json_data['cars'][val]['id'] for val in range(len(json_data['cars']))]
-    interventions = [json_data['interventions'][val]['id'] for val in range(len(json_data['interventions']))]
+    cars = [
+        json_data["cars"][val]["id"]
+        for val in range(len(json_data["cars"]))]
+    interventions = [
+        json_data["interventions"][val]["id"]
+        for val in range(len(json_data["interventions"]))
+    ]
     spent_time_by_op_results = []
     for val_car in cars:
         for val_interv in interventions:
-            results = requests.get(f'{URL}/{val_car}/{val_interv}')
+            results = requests.get(f"{url}/{val_car}/{val_interv}")
             time_load = json.loads(results.text)
-            time_load = time_load['labourTime']
-            spent_time_by_op_results.append({"car_id": val_car, "interv_id": val_interv, "time_spent": time_load})
-    return spent_time_by_op_results        
+            time_load = time_load["labourTime"]
+            spent_time_by_op_results.append(
+                                            {"car_id": val_car,
+                                             "interv_id": val_interv,
+                                             "time_spent": time_load}
+            )
+    return spent_time_by_op_results
 
 
-
-def services_charges(retrieve_hourly_rate,FILENAME):
-    with open(FILENAME,'r') as jsonfile:
+def services_charges(retrieve_hourly_rate, filename):
+    '''services_charges
+    change the hourly rate to a rate per minute,
+    for the workshops
+    input: data from retrieve_hourly_rate
+    output: dictionnary ->  car_id,
+                            workshop_id,
+                            price for the intervention,
+                            intervention_id,
+    '''
+    with open(filename, "r") as jsonfile:
         json_data = json.load(jsonfile)
 
-#change the hourly rate to a rate per minute, for the workshops
-    for val in range(len(json_data['workshops'])):
-        json_data['workshops'][val]['hourly_rate'] = (float(json_data['workshops'][val]['hourly_rate'])/60)
+    for val in range(len(json_data["workshops"])):
+        json_data["workshops"][val]["hourly_rate"] = (
+            float(json_data["workshops"][val]["hourly_rate"]) / 60
+        )
 
-
-#change the hh:mm:ss to a format with minutes e.g 1h20mn = 80mn
+    '''change the hh:mm:ss to a format with
+     minutes e.g 1h20mn = 80mn
+     '''
     for val_hour_rate in range(len(retrieve_hourly_rate)):
-        hours, minutes, seconds = [int(i) for i in retrieve_hourly_rate[val_hour_rate]['time_spent'].split(":")]
+        hours, minutes, seconds = [
+            int(i) for i in
+            retrieve_hourly_rate[val_hour_rate]["time_spent"].split(":")
+        ]
         total_minutes = hours * 60 + minutes
-        retrieve_hourly_rate[val_hour_rate]['time_spent'] = total_minutes
+        retrieve_hourly_rate[val_hour_rate]["time_spent"] = total_minutes
 
-#compute the prices for the intervention, per workshop according to the rates
-    services_prices_per_workshop= []
-    for val_workshop in range(len(json_data['workshops'])):
+    '''compute the prices for the intervention,
+    per workshop according to the rates
+    '''
+    services_prices_per_workshop = []
+    for val_workshop in range(len(json_data["workshops"])):
         for val_interv in range(len(retrieve_hourly_rate)):
 
-            val_service = retrieve_hourly_rate[val_interv]['time_spent']*json_data['workshops'][val_workshop]['hourly_rate']
-            services_prices_per_workshop.append({
-                "car_id": retrieve_hourly_rate[val_interv]['car_id'],
-                "workshop_id": json_data['workshops'][val_workshop]['id'],
-                "intervention_id": retrieve_hourly_rate[val_interv]['interv_id'],
-                "price_per_interv": val_service
-            })
+            val_service = (
+                retrieve_hourly_rate[val_interv]["time_spent"] *
+                json_data["workshops"][val_workshop]["hourly_rate"]
+            )
+            services_prices_per_workshop.append(
+                {
+                    "car_id": retrieve_hourly_rate[val_interv]["car_id"],
+                    "workshop_id": json_data["workshops"][val_workshop]["id"],
+                    "intervention_id":
+                        retrieve_hourly_rate[val_interv]["interv_id"],
+                    "price_per_interv": val_service,
+                }
+            )
     return services_prices_per_workshop
 
 
 def get_workshop_pref(services_charges_data):
+    '''get_workshop_pref
+    this function aim is is to add
+    the preference of the workshop
+    for the price part, either cheap, median or expensive
+    input: dictionnary from the function services_charges
+    output: same dictionnary with the preference of the workshop
+    '''
     for val_work in range(len(services_charges_data)):
-        #print(json_data['workshops'][val]['id'],json_data['workshops'][val]['preferred_part_price'])
-        if services_charges_data[val_work]['workshop_id'] == 1:
-            services_charges_data[val_work]['preffered'] = 'median'
-        elif services_charges_data[val_work]['workshop_id'] == 2:
-            services_charges_data[val_work]['preffered'] = 'most_expensive'
+        if services_charges_data[val_work]["workshop_id"] == 1:
+            services_charges_data[val_work]["preffered"] = "median"
+        elif services_charges_data[val_work]["workshop_id"] == 2:
+            services_charges_data[val_work]["preffered"] = "most_expensive"
         else:
-            services_charges_data[val_work]['preffered'] = 'cheapest'
-    return services_charges_data                     
+            services_charges_data[val_work]["preffered"] = "cheapest"
+    return services_charges_data
+
 
 def get_part(part_type, car_id, preffered):
-    with open(FILENAME,'r') as jsonfile:
-        json_data = json.load(jsonfile)    
-    prices = sorted([float(_["price"]) for _ in json_data["parts"] if _["type"] == part_type and _["car_id"] == car_id])
-    return prices[0] if preffered == "cheapest" else prices[-1] if preffered == "most_expensive" else prices[int(len(prices) / 2)]
+    """get_part
+    allows you to find the best price part, according
+    to the garage's preference.
+    input: json file given acting as a database
+    output: the price part, either cheap, median or
+    expensive
+    """
+    with open(filename, "r") as jsonfile:
+        json_data = json.load(jsonfile)
+    prices = sorted(
+        [
+            float(_["price"])
+            for _ in json_data["parts"]
+            if _["type"] == part_type and _["car_id"] == car_id
+        ]
+    )
+    return (
+        prices[0]
+        if preffered == "cheapest"
+        else prices[-1]
+        if preffered == "most_expensive"
+        else prices[int(len(prices) / 2)]
+    )
+
 
 def price_calculation(service_with_pref):
-    #service_with_pref = service_with_preference
+    """"price_calculation
+    calculate the price part, according to the type
+    if battery, just the price
+    if pad or disc, the prices are front brake plus
+    front pad, both are in pair.
+    input: dictionnary from get_workshop_pref
+    output: dictionnary with the part prices
+    """
     for val in range(len(service_with_pref)):
-        if service_with_pref[val]['intervention_id'] == 1:
-            parts_price = get_part('battery',service_with_pref[val]['car_id'],service_with_pref[val]['preffered'])
-            service_with_pref[val]['parts_price'] = parts_price
+        if service_with_pref[val]["intervention_id"] == 1:
+            parts_price = get_part(
+                "battery",
+                service_with_pref[val]["car_id"],
+                service_with_pref[val]["preffered"],
+            )
+            service_with_pref[val]["parts_price"] = parts_price
         else:
-            parts_price = get_part('front_brake_pad',service_with_pref[val]['car_id'],service_with_pref[val]['preffered']) * 2 + \
-                get_part('front_brake_disc',service_with_pref[val]['car_id'],service_with_pref[val]['preffered']) * 2
-            service_with_pref[val]['parts_price'] = parts_price  
-    #print(service_with_pref)           
-    return service_with_pref        
+            parts_price = (
+                get_part(
+                    "front_brake_pad",
+                    service_with_pref[val]["car_id"],
+                    service_with_pref[val]["preffered"],
+                ) * 2 +
+                get_part(
+                    "front_brake_disc",
+                    service_with_pref[val]["car_id"],
+                    service_with_pref[val]["preffered"],
+                ) * 2
+            )
+            service_with_pref[val]["parts_price"] = parts_price
+    return service_with_pref
 
 
+def parts(services_charges_data, filename_output):
+    """parts
+    change the intervention prices to a 2 digit, after decimal.
 
-def parts(services_charges_data,FILENAME_OUTPUT):
+
+    input: dictionnary from price_calculation
+    output: file, quotations.json, with a formatted dictionary.
+
+    """
     for val in range(len(final_services_charges_data)):
-        final_services_charges_data[val]['price_per_interv'] = "{:.2f}".format(final_services_charges_data[val]['price_per_interv'])
+        final_services_charges_data[val]["price_per_interv"] = "{:.2f}".format(
+            final_services_charges_data[val]["price_per_interv"]
+        )
 
-    with open(FILENAME_OUTPUT,'w') as jsonoutput:
-        jsonoutput.write(json.dumps(services_charges_data, sort_keys=True, indent=4))   
+    with open(filename_output, "w") as jsonoutput:
+        jsonoutput.write(json.dumps(services_charges_data,
+                         sort_keys=True, indent=4))
 
 
-if __name__ == '__main__':
-    retrieve_hourly_rate = retrieve_hourly_rate(URL,FILENAME)
-    services_charges_data = services_charges(retrieve_hourly_rate,FILENAME)
+if __name__ == "__main__":
+    retrieve_hourly_rate = retrieve_hourly_rate(url, filename)
+    services_charges_data = services_charges(retrieve_hourly_rate, filename)
     service_with_preference = get_workshop_pref(services_charges_data)
     final_services_charges_data = price_calculation(service_with_preference)
-    parts(final_services_charges_data,FILENAME_OUTPUT)
+    parts(final_services_charges_data, filename_output)
 
 
-
-
-''' expected output
+""" expected output
 
     {
         "car_id": 1, ok
@@ -121,4 +216,4 @@ if __name__ == '__main__':
     }
 
 
-'''
+"""
