888206cf46dd3484e756edebe39d09b693edd4d0
Andy Kwok <ext_kwok.andy@imsa.msa.fr>
2024-02-16 17:37:01 +0100

    MAJ - correction du premier test

--- test_functions.py
+++ test_functions.py
@@ -1,7 +1,13 @@
 """Creating 5 unitests for repairs_parts.py
 """
 
+import os
+
 import repairs_parts
+
+os.environ["URL_TO_FETCH"] = 'https://www.vroomly.com/backend_challenge/labour_times'
+os.environ["JSON_INPUT"] = 'data.json'
+os.environ["JSON_OUTPUT"] = 'quotations.json'
 
 def test_exp():
     url_to_fetch,json_input,json_output = repairs_parts.exp_variables_from_bash()    
